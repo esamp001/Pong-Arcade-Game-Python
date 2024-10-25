1913d60948633503bbea9824d1cfe645e034a4a9
esamp001 <ellejunsampaga@gmail.com>
2024-10-25 08:51:09 +0800

    new changes

--- ball.py
+++ ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move_ball(self):
         x = self.xcor() + self.x_move #move ball to x coordinate with speed of +10
@@ -22,10 +23,12 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def ball_reset(self):
-        self.home()
+        self.goto(0, 0)
         self.bounce_x()
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,35 +10,39 @@ screen.bgcolor("Black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-lpaddle = Paddle((350, 0))
-rpaddle = Paddle((-350, 0))
+lpaddle = Paddle((350, 0)) #Position of Paddle
+rpaddle = Paddle((-350, 0)) #Position of Paddle
 
 
 screen.listen()
-screen.onkey(rpaddle.move_up, "w")
-screen.onkey(rpaddle.move_down, "s")
-screen.onkey(lpaddle.move_up, "Up")
-screen.onkey(lpaddle.move_down, "Down")
+
+screen.onkeypress(rpaddle.move_up, "w")
+screen.onkeypress(rpaddle.move_down, "s")
+screen.onkeypress(lpaddle.move_up, "Up")
+screen.onkeypress(lpaddle.move_down, "Down")
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move_ball()
 
-    if ball.ycor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > 280 or ball.ycor() < -280: #Limit of y axis of ball
         ball.bounce_y()
 
-    if ball.distance(lpaddle) < 50 and ball.xcor() > 340 or ball.distance(rpaddle) < 50 and ball.xcor() < -340:
+    if ball.distance(lpaddle) < 50 and ball.xcor() > 320 or ball.distance(rpaddle) < 50 and ball.xcor() < -320: #ball and paddle collision
         ball.bounce_x()
 
-    if ball.xcor() > 380:
+    if ball.xcor() > 380: #Limit of X axis of ball
         ball.ball_reset()
+        scoreboard.l_point()
 
-    if ball.xcor() < -380:
+    if ball.xcor() < -380: #Limit of x axis of ball
         ball.ball_reset()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
